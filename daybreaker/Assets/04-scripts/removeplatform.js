4a34f44c85c03ffd2922d58f775d5665f986932a
James Stephens <jaystephens3.0@Gmail.com>
2015-03-02 15:39:03 -0500

    changes

--- daybreaker/Assets/04-scripts/removeplatform.js
+++ daybreaker/Assets/04-scripts/removeplatform.js
@@ -20,6 +20,15 @@ function OnTriggerEnter2D(other: Collider2D) {
 		Debug.Log(other.gameObject.rigidbody2D.velocity.y);
 		if (other.gameObject.rigidbody2D.velocity.y < -4) {
 			Debug.Log("break");
+			//iTween.MoveTo(destroyWhat,{
+			//	"x": other.gameObject.position.x + 3,
+			//	"time":4,
+			//	"delay":1,
+			//	"onupdate": "myUpdateFunction",
+			//	"looptype": "pingpong"
+			//});	
+			
+			iTween.ShakePosition(destroyWhat, Vector3(0.1, 0, 0), 2f);
 			Destroy(destroyWhat, 2);
 		} 
 	}
--- daybreaker/Assets/04-scripts/tunnel1 teleport.js
+++ daybreaker/Assets/04-scripts/tunnel1 teleport.js
@@ -16,9 +16,13 @@ function OnTriggerEnter2D(other: Collider2D) {
 	Debug.Log("collision");
 	if (other.gameObject.tag == "Player") {
 		Debug.Log("collision with player");
-		other.gameObject.rigidbody2D.velocity.x = 0;
-		other.gameObject.rigidbody2D.velocity.y = 0;
-		other.gameObject.transform.position = teleportTo.transform.position;	
+		
+		Debug.Log(other.gameObject.rigidbody2D.velocity.y);
+		if (other.gameObject.rigidbody2D.velocity.y < -3) {
+			other.gameObject.transform.position = teleportTo.transform.position;
+		}
+
+			
 		//Application.LoadLevel("level 1");
 	}
 }
